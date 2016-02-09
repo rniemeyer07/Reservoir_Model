3546af225334c0257154fec1e38cd100aa352119
rniemeyer07 <rniemeyr@uw.edu>
2016-02-09 15:44:41 -0800

    adding main program, subroutines, and Blocks

---
+++ reservoir.f90
@@ -0,0 +1,229 @@
+
+program reservoir
+!
+!------------------------------------------------------------------------- 
+!         Two-layer Reservoir River Temperature model
+!              University of Washington - Computational Hydrology Lab
+!         Authors: Ryan Niemeyer, Yifan Cheng, John Yearsley, and others
+!         Date: February 2016
+!
+!-------------------------------------------------------------------------
+
+!*************************************************************************
+!
+!    declare variables and initialize parameters
+!
+!*************************************************************************
+
+
+! use Block_Reservoir  
+use Block_Energy
+
+implicit none
+
+! --------------- allocate arrays ------------------
+allocate (Q_in(nd_total))
+allocate (stream_T_in(nd_total))
+allocate (headw_T_in(nd_total))
+allocate (Q_out(nd_total))
+allocate (stream_T_out(nd_total))
+allocate (headw_T_out(nd_total))
+allocate (air_T(nd_total))
+allocate (year(nd_total))
+allocate (month(nd_total))
+allocate (day(nd_total))
+
+! -------------------- to read in variables from comman line ---------
+!
+! Read total number of days to simulate JRY 
+!
+!  write(*,*) 'Total number of days of simulation'
+! read(*,*) nd_total
+
+ nd_total = 22645
+
+! Read some parameters
+!
+!  write(*,*) 'Input Inflow (m3/day),Inflow
+!  Temperature,temp_epil(1),temp_hypo(1),diffusion coefficient'
+!  read(*,*) Q_in_epil,Temp_in,temp_epil(1),temp_hypo(1),v_t
+
+! ----------------- read in input file -------
+! Note: the input_file should be entered with fortran executable:
+!   i.e. "two_layer_model input_file"
+! input_file contains paths to VIC files
+
+ call getarg (1, input_file)
+ input_file = TRIM(input_file)
+
+! ------------   data for the Cherokee Reservoir (36.1875, -83.4375)
+depth_total = 15 ! in meters
+depth_e = depth_total * 0.2
+depth_h = depth_total * 0.8
+width = 1377  ! in meters
+length = 86904 ! in meters
+area = width*length
+delta_t_sec = 84600.   ! Delta t in seconds
+delta_t = 1 ! time is days,  assumes all units in equations are in days
+
+flow_constant = 365/(2*Pi)
+! ------------------- initial variables ---------------
+volume_e_x = area*depth_e
+volume_h_x = area*depth_h
+ 
+temp_epil(1) = 15 ! starting epilimnion temperature at 5 C
+temp_hypo(1) = 15 ! starting hypolimnion temperature at 5 C
+v_t = 0.4  ! set the diffusion coeff.
+
+! -------------------- Upload files in input file -----------------
+! NOTE: once incorporated into RBM, these data will already be called in 
+!       the model 
+ OPEN(UNIT=45, FILE=TRIM(input_file), status='old')
+   read(45, *)  ! skip first line
+!
+! ----- read in inflow file -----
+  read(45, '(A)') inflow_file
+  open(unit=46, file=TRIM(inflow_file), ACCESS='SEQUENTIAL', FORM='FORMATTED',
+STATUS='old')
+
+! ------- read in outflow file -------
+  read(45, '(A)') outflow_file
+  open(unit=47, file=TRIM(outflow_file), ACCESS='SEQUENTIAL', FORM='FORMATTED',
+STATUS='old')
+
+! ------- read in energy file -------
+  read(45, '(A)') energy_file
+  open(unit=48, file=TRIM(energy_file), ACCESS='SEQUENTIAL', FORM='FORMATTED',
+STATUS='old')
+!
+! ------- read in observed stream temperature file -------
+  read(45, '(A)') observed_stream_temp_file
+  open(unit=49, file=TRIM(observed_stream_temp_file), ACCESS='SEQUENTIAL',
+FORM='FORMATTED' &
+         ,  STATUS='old')
+
+
+
+!*************************************************************************
+!
+!       run through loop
+!
+!*************************************************************************
+
+! -------- set initial temperatures -------------
+t1 = 15
+t2 = 15
+
+! ------------ start loop ---------------------
+do  nd=1,nd_total
+
+
+
+!*************************************************************************
+! read forcings for energy and flow from VIC and RVIC
+!*************************************************************************
+
+      ! ------------------ read in in VIC flow data --------------
+        read(46, *) year(i),month(i),day(i), Q_in(i) &
+              , stream_T_in(i), headw_T_in(i), air_T(i)
+
+        flow_in_hyp_x = Q_in(i)*prcnt_flow_epil
+        flow_in_epi_x = Q_in(i)*prcnt_flow_hypo
+
+     ! ---------- set outflow to inflow (same out as in)  ---
+         read(47, *) year(i),month(i),day(i), Q_out(i), stream_T_out(i) &
+            ,  headw_T_out(i), air_T(i)
+
+!       Q_in(i) = Q_in_epil
+!       ! For test
+
+        flow_out_hyp_x = Q_out(i)*prcnt_flow_hypo
+        flow_out_epi_x = Q_out(i)*prcnt_flow_epil
+
+      ! ------------- flow between epilim. and hypolim. ---------
+        flow_epi_hyp_x = flow_in_epi_x
+
+      ! ------------- read in stream temperature data --------
+
+      !  Using value of inflow from command line JRY
+      !
+      !     flow_Tin(i) = Temp_in
+
+      ! ----------------- read in from VIC data -------------------
+
+      flow_Tin(i) = stream_T_in(i)
+
+!*************************************************************************
+! read forcings for energy from VIC
+!*************************************************************************
+
+    ! ------- ulpload ENERGY subroutine and read in VIC energy ----
+        ! read(48, *) year(i),month(i),day(i), Q_in(i), stream_T_in(i)  &
+        !        ,  headw_T_in(i), air_T(i)
+
+    ! call energy(T_0,q_surf,nncell)
+            q_dot=(q_surf/(z*rfac))
+            T_0=T_0+q_dot*dt_calc
+            if(T_0.lt.0.0) T_0=0.0
+
+
+       x = i
+      energy_x  =  cos(((x)/flow_constant)+ Pi )
+!
+!      energy_x = 0      ! Surface flux set to zero for testing JRY
+!
+      energy_x =( (energy_x)*150)*delta_t_sec !converts to Joules/m2 * day
+      energy_x = energy_x*area
+
+
+!***********************************************************************
+! read flow schedule (spill and turbine outflows)
+!*************************************************************************
+
+     ! ---------- set outflow to inflow (same out as in)  ---
+         read(47, *) year(i),month(i),day(i), Q_out(i), stream_T_out(i) &
+            ,  headw_T_out(i), air_T(i)
+
+
+
+!*************************************************************************
+!      read inflow and river temperature from rbm simulations
+!*************************************************************************
+
+
+!*************************************************************************
+!      call reservoir subroutine
+!*************************************************************************
+
+   call reservoir_subroutine (ALL PARAMETERS AND VARIABLES needed to SIMULATE TEMPERATURES)
+
+
+
+
+!*************************************************************************
+!          write output
+!*************************************************************************
+
+
+    write(30,*) i,temp_epil(i),temp_hypo(i),temp_out_tot(i), flow_Tin(i) &
+               , temp_change_ep(i), temp_change_hyp(i),advec_in_hypxi &
+               , advec_out_hypx,dV_dt_hyp, flow_epi_hyp_x, volume_e_x & 
+               , volume_h_x, flow_in_epi_x, flow_out_hyp_x
+
+
+!*************************************************************************
+!               update temperatures 
+!*************************************************************************
+
+!
+! Update temperatures for next time step, for example:
+         temp_epil(1) = temp_epil(2)
+         temp_hypo(1) = temp_hypo(2)
+! Or, better (1=epilimnion,2=hypolimnion)
+         T_res(1,1)   = T_res(1,2)
+         T_res(2,1)   = T_res(2,2)
+!
+end do
+
+
+end program reservoir
---
+++ reservoir_subroutine.f90
@@ -0,0 +1,123 @@
+SUBROUTINE reservoir_subroutine(variables that need to be passed)
+   use Block_Routine
+
+
+
+  ! ------------ calculate temperature change due to diffusion ---------------
+      ! NOTE: don't need to multiply by heat capacity or density of water
+      ! because
+      !       energy component is divided by those two
+
+      dif_epi_x  = v_t * area * density * heat_c * (temp_hypo(i-1) -
+temp_epil(i-1))
+      dif_hyp_x  = v_t * area * density * heat_c * (temp_epil(i-1) -
+temp_hypo(i-1))
+
+  ! ------------------------- calculate advection --------------------------- 
+
+    ! ---------------- epilimnion -------------
+         advec_in_epix  = flow_in_epi_x * density * heat_c * (flow_Tin(i))
+         advec_out_epix = flow_out_epi_x * density * heat_c * (temp_epil(i-1))
+         advec_epi_hyp = flow_epi_hyp_x * density * heat_c * (temp_epil(i-1))
+         advec_out_epix = advec_out_epix + advec_epi_hyp
+
+     ! ---------------- hypolimnion ------------
+         advec_in_hypx = flow_in_hyp_x * density * heat_c * (flow_Tin(i))
+         advec_in_hypx = advec_in_hypx + advec_epi_hyp
+         advec_out_hypx = flow_out_hyp_x * density * heat_c * (temp_hypo(i-1))
+
+  ! ------------------------- calculate dV/dt ---------------------------
+    ! ---------------- epilimnion -------------
+         dV_dt_epi = (flow_in_epi_x - flow_out_epi_x - flow_epi_hyp_x) * density
+* heat_c * (temp_epil(i-1))
+
+
+    ! ---------------- hypolimnion ------------
+         dV_dt_hyp = (flow_in_hyp_x + flow_epi_hyp_x - flow_out_hyp_x)  *
+density * heat_c * (temp_hypo(i-1))
+
+   ! ------------------- calculate change in temperature  ---------------------
+
+       ! ---------------- epilimnion -----------
+
+          !----- calculate change in layer volume  -------
+             delta_vol_e_x = flow_in_epi_x - flow_out_epi_x - flow_epi_hyp_x
+             delta_vol_e_T_x = (-1) * density * heat_c * (temp_epil(i-1)) * &
+                         delta_vol_e_x / delta_t
+
+            ! ------------ calculate total energy ----------
+            temp_change_ep(i) = advec_in_epix - advec_out_epix  + energy_x +
+dif_epi_x - dV_dt_epi
+!  write(*,*) advec_in_epix, advec_out_epix, energy_x, dif_epi_x
+! write(*,*) dif_epi_x, temp_hypo(i-1), temp_change_hyp(i) 
+            ! loop to calculate volume if Qout > volume
+             if (flow_out_epi_x > volume_e_x) then
+              vol_x = flow_in_epi_x
+             else if (flow_out_epi_x < volume_e_x) then
+              vol_x = volume_e_x
+             end if
+
+           temp_change_ep(i) = temp_change_ep(i)/(vol_x * density * heat_c)
+           temp_change_ep(i) = temp_change_ep(i) * delta_t
+
+            !----- update epilimnion volume for next time step -------
+              volume_e_x = volume_e_x + (flow_in_epi_x - flow_out_epi_x)
+              temp_epil(i) = temp_epil(i-1) +  temp_change_ep(i)
+
+
+            ! -------- save each energy component ------
+             energy_tot(i) = energy_x
+             diffusion_tot(i) = dif_epi_x
+             T_in_tot(i) = flow_in_epi_x*flow_Tin(i)/volume_e_x
+             T_out_tot(i) = flow_out_epi_x*temp_epil(i-1)/volume_e_x
+
+       ! ------------------ hypolimnion ----------------
+
+          !----- calculate change in layer volume  -------
+           delta_vol_h_x = flow_in_hyp_x - flow_out_hyp_x + flow_epi_hyp_x
+           delta_vol_h_T_x = (-1) * density * heat_c * (temp_hypo(i-1)) * &
+                        delta_vol_h_x / delta_t
+
+          ! ------------ calculate total energy ----------
+            temp_change_hyp(i) = advec_in_hypx -  advec_out_hypx  +  dif_hyp_x
+-dV_dt_hyp
+
+            ! loop to calculate temperature change with Qin, IF Qout >
+            ! volume_h_x
+             if (flow_out_hyp_x > volume_h_x) then
+               vol_x = flow_epi_hyp_x
+             else if (flow_out_hyp_x < volume_h_x) then
+                vol_x = volume_h_x
+             end if
+
+            !   write(*,*) volume_h_x, vol_x, flow_out_hyp_x,flow_epi_hyp_x,
+            !   temp_change_hyp(i)
+
+           temp_change_hyp(i) = temp_change_hyp(i)/(vol_x * density * heat_c)
+           temp_change_hyp(i) = temp_change_hyp(i) * delta_t
+
+          !----- update epilimnion volume for next time step -------
+           volume_h_x = volume_h_x + flow_in_hyp_x - flow_out_hyp_x +
+flow_epi_hyp_x
+           temp_hypo(i) = temp_hypo(i-1) +  temp_change_hyp(i)
+!
+! Print output to unit = 30 JRY
+!
+
+
+  !---------- calculate combined (hypo. and epil.) temperature of outflow -----
+    outflow_x = flow_out_epi_x + flow_out_hyp_x
+    epix = temp_epil(i)*(flow_out_epi_x/outflow_x)  ! portion of temperature
+from epilim. 
+    hypox= temp_hypo(i)*(flow_out_hyp_x/outflow_x)  ! portion of temperature
+from hypol.
+    temp_out_tot(i) = epix + hypox
+
+!------------- write energy budget terms each time step and data to save -----
+          write(*,*) dif_epi_x, temp_hypo(i-1), temp_change_hyp(i),advec_in_hypx &
+                 ,  advec_out_hypx,  dif_hyp_x
+
+          write(30,*) i,temp_epil(i),temp_hypo(i),temp_out_tot(i), flow_Tin(i) &
+               ,  temp_change_ep(i), temp_change_hyp(i), advec_in_hypx,advec_out_hypx,dV_dt_hyp &
+                , flow_epi_hyp_x, volume_e_x, volume_h_x, flow_in_epi_x,flow_out_hyp_x
+
