1206112dbaa2c16c95193521193dc9603361605a
Yifan Cheng <yifan@hydra.hydro.washington.edu>
2016-02-18 12:46:39 -0800

    Make the code run

--- reservoir_subroutine.f90
+++ reservoir_subroutine.f90
@@ -1,10 +1,9 @@
-SUBROUTINE reservoir_subroutine(T_epil_temp,T_hypo_temp, volume_e_x,volume_h_x,i)
+SUBROUTINE reservoir_subroutine(T_epil_temp,T_hypo_temp, volume_e_x,volume_h_x)
    use Block_Reservoir
 
 implicit none
 
 real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
-integer :: i
 
   ! ------------ calculate temperature change due to diffusion ---------------
       ! NOTE: don't need to multiply by heat capacity or density of water
@@ -17,13 +16,13 @@ integer :: i
   ! ------------------------- calculate advection --------------------------- 
 
     ! ---------------- epilimnion -------------
-         advec_in_epix  = flow_in_epi_x * density * heat_c * (flow_Tin(i))
+         advec_in_epix  = flow_in_epi_x * density * heat_c * (stream_T_in)
          advec_out_epix = flow_out_epi_x * density * heat_c * (T_epil_temp)
          advec_epi_hyp = flow_epi_hyp_x * density * heat_c * (T_epil_temp)
          advec_out_epix = advec_out_epix + advec_epi_hyp
 
      ! ---------------- hypolimnion ------------
-         advec_in_hypx = flow_in_hyp_x * density * heat_c * (flow_Tin(i))
+         advec_in_hypx = flow_in_hyp_x * density * heat_c * (stream_T_in)
          advec_in_hypx = advec_in_hypx + advec_epi_hyp
          advec_out_hypx = flow_out_hyp_x * density * heat_c * (T_hypo_temp)
 
@@ -45,7 +44,7 @@ integer :: i
                          delta_vol_e_x / delta_t
 
             ! ------------ calculate total energy ----------
-            temp_change_ep(i) = advec_in_epix - advec_out_epix  + energy_x + dif_epi_x - dV_dt_epi
+            temp_change_ep = advec_in_epix - advec_out_epix  + energy_x + dif_epi_x - dV_dt_epi
 !  write(*,*) advec_in_epix, advec_out_epix, energy_x, dif_epi_x
 ! write(*,*) dif_epi_x, temp_hypo(i-1), temp_change_hyp(i) 
             ! loop to calculate volume if Qout > volume
@@ -55,20 +54,20 @@ integer :: i
               vol_x = volume_e_x
             ! end if
 
-           temp_change_ep(i) = temp_change_ep(i)/(vol_x * density * heat_c)
-           temp_change_ep(i) = temp_change_ep(i) * delta_t
+           temp_change_ep = temp_change_ep/(vol_x * density * heat_c)
+           temp_change_ep = temp_change_ep * delta_t
 
             ! -------- save each energy component ------
-             energy_tot(i) = energy_x
-             diffusion_tot(i) = dif_epi_x
-             T_in_tot(i) = flow_in_epi_x*flow_Tin(i)/volume_e_x
-             T_out_tot(i) = flow_out_epi_x*T_epil_temp/volume_e_x
+             energy_tot = energy_x
+             diffusion_tot = dif_epi_x
+             T_in_tot = flow_in_epi_x*stream_T_in/volume_e_x
+             T_out_tot = flow_out_epi_x*T_epil_temp/volume_e_x
 
 
 
             !----- update epilimnion volume for next time step -------
               volume_e_x = volume_e_x + (flow_in_epi_x - flow_out_epi_x + flow_epi_hyp_x ) *delta_t
-              T_epil_temp = T_epil_temp +  temp_change_ep(i)
+              T_epil_temp = T_epil_temp +  temp_change_ep
 
 
        ! ------------------ hypolimnion ----------------
@@ -79,7 +78,7 @@ integer :: i
                         delta_vol_h_x / delta_t
 
           ! ------------ calculate total energy ----------
-            temp_change_hyp(i) = advec_in_hypx -  advec_out_hypx  +  dif_hyp_x -dV_dt_hyp
+            temp_change_hyp = advec_in_hypx -  advec_out_hypx  +  dif_hyp_x -dV_dt_hyp
 
             ! loop to calculate temperature change with Qin, IF Qout >
             ! volume_h_x
@@ -92,12 +91,12 @@ integer :: i
             !   write(*,*) volume_h_x, vol_x, flow_out_hyp_x,flow_epi_hyp_x,
             !   temp_change_hyp(i)
 
-           temp_change_hyp(i) = temp_change_hyp(i)/(vol_x * density * heat_c)
-           temp_change_hyp(i) = temp_change_hyp(i) * delta_t
+           temp_change_hyp = temp_change_hyp/(vol_x * density * heat_c)
+           temp_change_hyp = temp_change_hyp * delta_t
 
           !----- update epilimnion volume for next time step -------
            volume_h_x = volume_h_x + (flow_in_hyp_x - flow_out_hyp_x + flow_epi_hyp_x)*delta_t
-           T_hypo_temp = T_hypo_temp +  temp_change_hyp(i)
+           T_hypo_temp = T_hypo_temp +  temp_change_hyp
 !
 ! Print output to unit = 30 JRY
 !
@@ -107,7 +106,7 @@ integer :: i
     outflow_x = flow_out_epi_x + flow_out_hyp_x
     epix = T_hypo_temp*(flow_out_epi_x/outflow_x)  ! portion of temperature from epilim. 
     hypox= T_hypo_temp*(flow_out_hyp_x/outflow_x)  ! portion of temperature from hypol.
-    temp_out_tot(i) = epix + hypox
+    temp_out_tot = epix + hypox
 
 !------------- write energy budget terms each time step and data to save -----
 !          write(*,*) dif_epi_x, temp_hypo, temp_change_hyp(i),advec_in_hypx &
