8179698ca2432df1f9063641a895fb08236c4c89
rniemeyer07 <rniemeyr@uw.edu>
2016-02-22 12:53:41 -0800

    small changes to annotation

--- reservoir_subroutine.f90
+++ reservoir_subroutine.f90
@@ -5,11 +5,11 @@ implicit none
 
 real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
 
-  ! ------------ calculate temperature change due to diffusion ---------------
+  ! -------------------- calculate temperature term  -------------------------
       dif_epi_x  = v_t * area * density * heat_c * (T_hypo_temp - T_epil_temp)
       dif_hyp_x  = v_t * area * density * heat_c * (T_epil_temp - T_hypo_temp)
 
-  ! ------------------------- calculate advection --------------------------- 
+  ! --------------------- calculate advection term --------------------------- 
          advec_in_epix  = flow_in_epi_x * density * heat_c * (stream_T_in)
          advec_out_epix = flow_out_epi_x * density * heat_c * (T_epil_temp)
          advec_epi_hyp = flow_epi_hyp_x * density * heat_c * (T_epil_temp)
@@ -34,11 +34,11 @@ real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
               vol_x = volume_e_x
             end if
 
-           temp_change_ep = temp_change_ep/(vol_x * density * heat_c)
-           temp_change_ep = temp_change_ep * delta_t
-          !----- update epilimnion volume for next time step -------
-            volume_e_x = volume_e_x + (flow_in_epi_x - flow_out_epi_x + flow_epi_hyp_x ) *delta_t
-            T_epil_temp = T_epil_temp +  temp_change_ep
+          temp_change_ep = temp_change_ep/(vol_x * density * heat_c)
+          temp_change_ep = temp_change_ep * delta_t
+         !----- update epilimnion volume for next time step -------
+          volume_e_x = volume_e_x + (flow_in_epi_x - flow_out_epi_x + flow_epi_hyp_x ) *delta_t
+          T_epil_temp = T_epil_temp +  temp_change_ep
 
       ! ------------------ hypolimnion ----------------
          ! ------------ calculate total energy ----------
@@ -51,11 +51,11 @@ real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
                vol_x = volume_h_x
             end if
 
-           temp_change_hyp = temp_change_hyp/(vol_x * density * heat_c)
-           temp_change_hyp = temp_change_hyp * delta_t
-          !----- update epilimnion volume for next time step -------
-           volume_h_x = volume_h_x + (flow_in_hyp_x - flow_out_hyp_x + flow_epi_hyp_x)*delta_t
-           T_hypo_temp = T_hypo_temp +  temp_change_hyp
+          temp_change_hyp = temp_change_hyp/(vol_x * density * heat_c)
+          temp_change_hyp = temp_change_hyp * delta_t
+         !----- update epilimnion volume for next time step -------
+          volume_h_x = volume_h_x + (flow_in_hyp_x - flow_out_hyp_x + flow_epi_hyp_x)*delta_t
+          T_hypo_temp = T_hypo_temp +  temp_change_hyp
 
   !---------- calculate combined (hypo. and epil.) temperature of outflow -----
     outflow_x = flow_out_epi_x + flow_out_hyp_x
