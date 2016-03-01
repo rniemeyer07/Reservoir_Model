959b3075d6806995ff1a80b1a1cfeb51d6501366
rniemeyer07 <rniemeyr@uw.edu>
2016-02-29 16:44:13 -0800

    edited some of the commented text

--- reservoir_subroutine.f90
+++ reservoir_subroutine.f90
@@ -5,11 +5,11 @@ implicit none
 
 real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
 
-  ! -------------------- calculate temperature term  -------------------------
+  ! -------------------- calculate temperature terms  -------------------------
       dif_epi_x  = v_t * area * density * heat_c * (T_hypo_temp - T_epil_temp)
       dif_hyp_x  = v_t * area * density * heat_c * (T_epil_temp - T_hypo_temp)
 
-  ! --------------------- calculate advection term --------------------------- 
+  ! --------------------- calculate advection terms --------------------------- 
          advec_in_epix  = flow_in_epi_x * density * heat_c * (stream_T_in)
          advec_out_epix = flow_out_epi_x * density * heat_c * (T_epil_temp)
          advec_epi_hyp = flow_epi_hyp_x * density * heat_c * (T_epil_temp)
@@ -18,7 +18,7 @@ real :: volume_e_x, volume_h_x, T_epil_temp, T_hypo_temp
          advec_in_hypx = advec_in_hypx + advec_epi_hyp
          advec_out_hypx = flow_out_hyp_x * density * heat_c * (T_hypo_temp)
 
-  ! ------------------------- calculate dV/dt term ---------------------------
+  ! ------------------------- calculate dV/dt terms ---------------------------
          dV_dt_epi = (flow_in_epi_x - flow_out_epi_x - flow_epi_hyp_x) * density * heat_c * (T_epil_temp)
          dV_dt_hyp = (flow_in_hyp_x + flow_epi_hyp_x - flow_out_hyp_x) * density * heat_c * (T_hypo_temp)
 
